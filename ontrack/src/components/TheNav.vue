9f35bd9afe556e9b0c9ad3250ab2fa3b38c7d5d0
Daniil Boyko <d.boyko@mwnts.ru>
2024-05-28 15:31:45 +0300

    feat: added nav menu handler

--- ontrack/src/components/TheNav.vue
+++ ontrack/src/components/TheNav.vue
@@ -1,21 +1,29 @@
 <script setup>
+import { ref } from 'vue'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import NavItem from './NavItem.vue'
 
 const navItems = {
   timeline: ClockIcon,
   activities: ListBulletIcon,
   stats: ChartBarIcon
 }
+
+const currentPage = ref('timeline')
 </script>
 
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <li v-for="(icon, page) in navItems" class="flex-1">
-        <a :href="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize">
-          <component :is="icon" class="h-6 w-6" /> {{ page }}
-        </a>
-      </li>
+      <NavItem
+        v-for="(icon, page) in navItems"
+        :key="page"
+        :href="`#${page}`"
+        :class="{ 'bg-gray-200 pointer-events-none': currentPage === page }"
+        @click="currentPage = page"
+      >
+        <component :is="icon" class="h-6 w-6" /> {{ page }}
+      </NavItem>
     </ul>
   </nav>
 </template>
