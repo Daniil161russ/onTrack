187618f95f4d9cde55a4798c8ceb85f611937841
Daniil Boyko <d.boyko@mwnts.ru>
2024-05-28 15:10:03 +0300

    feat: added nav template

--- ontrack/src/App.vue
+++ ontrack/src/App.vue
@@ -1,5 +1,12 @@
 <script setup>
 import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+
+const navItems = {
+  timeline: ClockIcon,
+  activities: ListBulletIcon,
+  stats: ChartBarIcon
+}
 </script>
 
 <template>
@@ -20,4 +27,16 @@ import { CheckCircleIcon } from '@heroicons/vue/24/solid'
       </div>
     </a>
   </header>
+
+  <main class="flex flex-grow flex-col"></main>
+
+  <nav class="sticky bottom-0 z-10 bg-white">
+    <ul class="flex items-center justify-around border-t">
+      <li v-for="icon, page in navItems" class="flex-1">
+        <a :href="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize">
+          <component :is="icon" class="h-6 w-6" /> {{ page }}
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
